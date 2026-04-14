e799471ddbed042609d0b8943d3ab4369c3e9738
marcopogi123 <qmtsalva@tip.edu.ph>
2026-04-14 16:13:20 +0800

    Delete Flutter/salva_practice_exercise_5_1 directory

--- Flutter/salva_practice_exercise_5_1/README.md
+++
@@ -1,3 +0,0 @@
-# salva_practice_exercise_5_1
-
-A new Flutter project.
--- Flutter/salva_practice_exercise_5_1/android/app/src/main/kotlin/com/example/salva_practice_exercise_5_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.salva_practice_exercise_5_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
